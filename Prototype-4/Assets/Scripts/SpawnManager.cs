140d9d29a086733960154fef032000710c8b5fca
machen18 <machen18@s.sfusd.edu>
2023-03-14 14:10:26 -0700

    Destroying enemies + enemy waves

--- Prototype-4/Assets/Scripts/SpawnManager.cs
+++ Prototype-4/Assets/Scripts/SpawnManager.cs
@@ -6,17 +6,25 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3); 
+        SpawnEnemyWave(waveNumber); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
